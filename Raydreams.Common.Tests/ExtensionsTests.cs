b30da95bfa55ff1479773c7cda4086c743de562e
Grumpy Cockatiel <tguillory@gmail.com>
2021-01-21 18:54:33 -0600

    added extension

---
+++ Raydreams.Common.Tests/ExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Raydreams.Common.Extensions;
+
+namespace Raydreams.Common.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void PairsToDictionaryTest()
+        {
+            string auth = "Digest realm=\"MMS Public API\", domain=\"\", nonce=\"kWVA9Ciu7lNaN5QdjPe8kxPMReVjbt+B\", algorithm=MD5, qop=\"auth\", stale=false";
+
+            Dictionary<string, string> results = auth.PairsToDictionary();
+
+            Assert.IsNotNull( results );
+        }
+    }
+}
--- Raydreams.Common/Extensions/StringExtensions.cs
+++ Raydreams.Common/Extensions/StringExtensions.cs
@@ -309,6 +309,37 @@ namespace Raydreams.Common.Extensions
             return temp.ToString();
         }
 
+        /// <summary>Given a string in the format key1=value1,key2=value2,key3=value3 splits into a dictionary</summary>
+        public static Dictionary<string,string> PairsToDictionary( this string str, bool stripQuotes = true )
+        {
+            Dictionary<string, string> results = new Dictionary<string, string>();
+
+            StringBuilder temp = new StringBuilder();
+
+            foreach ( char c in str )
+            {
+                if ( c == ',' )
+                {
+                    string[] parts = temp.ToString().Split( '=', StringSplitOptions.None );
+                    if ( parts != null && parts.Length > 0 && !String.IsNullOrWhiteSpace(parts[0]) )
+                    {
+                        parts[1] = ( parts.Length < 2 || String.IsNullOrWhiteSpace( parts[1] ) ) ? String.Empty : parts[1].Trim();
+                        if ( stripQuotes )
+                            parts[1] = parts[1].Replace( "\"", "" );
+                        results.Add( parts[0].Trim(), parts[1] );
+                    }
+                        
+                    temp = new StringBuilder();
+                }
+                else
+                {
+                    temp.Append( c );
+                }
+            }
+
+            return results;
+        }
+
         /// <summary>Cleans a string of all non digit characters and collapses to a string with no spaces.</summary>
         public static string RemoveNonDigits( this string str )
 		{
